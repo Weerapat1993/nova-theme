c5a9ec8fa007846a3595f0bd006d4470be242ba1
Top <weerapat.thawatchoketawee@gmail.com>
2017-10-03 00:10:10 +0700

    Add Comment

--- src/utils/matchKeyObject/matchKeyObject.js
+++ src/utils/matchKeyObject/matchKeyObject.js
@@ -29,12 +29,31 @@ export const matchKeyObject = (state, key, data) => ({
  */
 export const hasObject = (data, field) => _.has(data, field)
 
+/**
+ * Test Reducer Class Method
+ * ```js
+ * const state = new TestReducer(initalState)
+ * ```
+ */
 export class TestReducer {
+  /**
+   * set InitalState in Reducer
+   * ```js
+   * const state = new TestReducer(initalState)
+   * ```
+   * @param {object} initalState 
+   */
   constructor(initalState) {
     this.oldState = initalState
     this.state = initalState
   }
 
+  /**
+   * Set New State in Reducer
+   * @param {string} key 
+   * @param {object} data
+   * @return {object}
+   */
   setData(key, data) {
     this.oldState = this.state
     this.state = {
@@ -47,10 +66,18 @@ export class TestReducer {
     return this.getState()
   }
 
+  /**
+   * Get state after change data
+   * @return {object}
+   */
   getState() {
     return this.state
   }
 
+  /**
+   * Get state before change data
+   * @return {object}
+   */
   getOldState() {
     return this.oldState
   }
